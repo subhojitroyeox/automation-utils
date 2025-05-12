f5cdb58c0c1264f549f592b349479b3041a17233
Subhojit Roy <subhojitr@corp.eoxvantage.com>
2025-05-12 13:29:39 +0530

    this function will update the selectItemFromDropdown2

--- src/main/java/com/eox/utils/CommonFunctionUtils.java
+++ src/main/java/com/eox/utils/CommonFunctionUtils.java
@@ -89,18 +89,26 @@ public class CommonFunctionUtils {
  // select function
     public static void selectItemFromDropdown(String dropdownItem, String dropdownMenuItem) {
     	SupportUtils.waitFor(200);
+    	elementClick(driver.findElement(By.xpath("//*[contains(text(),'"+dropdownItem+"')]/..//div[contains(@class,'choices')]")));
+    	
+    	try{
+    		elementClick(driver.findElement(By.xpath("//*[text()='"+dropdownMenuItem+"']/..")));
+    	}
+    	catch (Exception e) {
+    		SupportUtils.waitFor(200);
+    		elementClick(driver.findElement(By.xpath("//*[contains(text(),'"+dropdownItem+"')]/..//div[contains(@class,'choices')]")));
+    		
+    		elementClick(driver.findElement(By.xpath("//*[contains(text(),'"+dropdownMenuItem+"')]/../..")));
+    		
+    	}
+    	
+    }
+    public static void selectItemFromDropdown2(String dropdownItem, String dropdownMenuItem) {
+    	SupportUtils.waitFor(200);
  		elementClick(driver.findElement(By.xpath("//*[contains(text(),'"+dropdownItem+"')]/..//div[contains(@class,'choices')]")));
  		
- 		try{
- 			elementClick(driver.findElement(By.xpath("//*[text()='"+dropdownMenuItem+"']/..")));
- 		}
- 		catch (Exception e) {
- 			SupportUtils.waitFor(200);
- 			elementClick(driver.findElement(By.xpath("//*[contains(text(),'"+dropdownItem+"')]/..//div[contains(@class,'choices')]")));
- 			
- 			elementClick(driver.findElement(By.xpath("//*[contains(text(),'"+dropdownMenuItem+"')]/../..")));
- 			
- 		}
+ 			elementClick(driver.findElement(By.xpath("//*[contains(text(),'"+dropdownItem+"')]/..//*[text()='"+dropdownMenuItem+"']/..")));
+ 		
  		
  	}
  	
